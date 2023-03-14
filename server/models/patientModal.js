68881bbd05f075353d607311d044410de34405fe
enbarSoco <enbar2232@gmail.com>
2023-03-14 21:27:47 +0200

    Revert "Revert "fixed patient schema""
    This reverts commit 5817e1d2764592232732ee1bd025ae669a7a8181.

--- server/models/patientModal.js
+++ server/models/patientModal.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
+const goalSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  goalType: { type: String, enum: ["regular", "bonus"] },
+});
+
 const patientSchema = new mongoose.Schema({
-  name: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-  },
-  tokens: {
-    amount: { type: Number, default: 0 },
-    description: { type: String },
-  },
+  fullName: { type: String, required: true },
+  id: { type: Number, required: true },
+  age: { type: Number, required: true },
+  tokens: { type: Number, default: 0 },
+  goals: { type: [goalSchema] },
   createdAt: { type: Date, default: new Date() },
 });
 const patientModal = mongoose.model("patient", patientSchema);
