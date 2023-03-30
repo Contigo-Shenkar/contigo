a10f760c8b97afd7b9609a19b468d56b2ef80bba
moram16 <moramihay@gmail.com>
2023-03-31 01:04:53 +0300

    adding avatars&update patientInfo

--- client/src/components/patientInfo/patientInfo.js
+++ client/src/components/patientInfo/patientInfo.js
@@ -1,6 +1,6 @@
 import Header from "../../components/header/Header";
 import { tokens } from "../../theme";
-import { DataGrid } from '@mui/material';
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import {alertNotification} from "../alert/alertNotification";
 import {useGetPatientByIdQuery}  from "../../features/apiSlice";
 
@@ -13,7 +13,8 @@ import { Box,
     CardContent, 
     Grid,
     Button,
-    CardActions,
+    CardActions
+  
     
     
     
@@ -21,7 +22,8 @@ import { Box,
 
 
 const PatientInfo = ()=> {
-    const { data: patients, isLoading, isError } = useGetPatientByIdQuery();
+    const { data: patients, isLoading, isError } = useGetPatientByIdQuery({id:"6416311dee42cbbe6828b246"});
+    console.log(patients);
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
   
@@ -32,7 +34,160 @@ const PatientInfo = ()=> {
         name: 'Netanel Admoni',
         Department: 'Department D Child Psychiatry'
       }
+      
+  const patientsWithCompletedTasksPercent = patients?.data.map((patient) => {
+    const openTasksCount = patient.tasks.reduce(
+      (total, task) => (task.status === "run" ? total + 1 : total),
+      0
+    );
+    const completedTasks = patient.tasks.reduce(
+      (total, task) => (task.status === "completed" ? total + 1 : total),
+      0
+    );
+    const completedRegularTasks = patient.tasks.reduce(
+      (total, task) =>
+        task.status === "completed" && task.type === "regular"
+          ? total + 1
+          : total,
+      0
+    );
+    const completedBonusTasks = patient.tasks.reduce(
+      (total, task) =>
+        task.status === "completed" && task.type === "bonus"
+          ? total + 1
+          : total,
+      0
+    );
+    const totalRegularTasks = patient.tasks.filter(
+      (task) => task.type === "regular"
+    ).length;
+    const totalBonusTasks = patient.tasks.filter(
+      (task) => task.type === "bonus"
+    ).length;
+    const completedTasksPercent =
+      patient.tasks.length > 0
+        ? (completedTasks / patient.tasks.length) * 100
+        : 0;
+    const completedRegularTasksPercent =
+      totalRegularTasks > 0
+        ? (completedRegularTasks / totalRegularTasks) * 100
+        : 0;
+    const completedBonusTasksPercent =
+      totalBonusTasks > 0 ? (completedBonusTasks / totalBonusTasks) * 100 : 0;
 
+    const isSuccessful =
+      completedRegularTasksPercent >= 80 && completedBonusTasksPercent >= 20;
+
+    const canStillSucceedRegularTasks =
+      (completedRegularTasks +
+        (totalRegularTasks -
+          completedRegularTasks -
+          patient.tasks.filter(
+            (task) => task.type === "regular" && task.status === "not-completed"
+          ).length)) /
+        totalRegularTasks >=
+      0.8;
+
+    const canStillSucceedBonusTasks =
+      (completedBonusTasks +
+        (totalBonusTasks -
+          completedBonusTasks -
+          patient.tasks.filter(
+            (task) => task.type === "bonus" && task.status === "not-completed"
+          ).length)) /
+        totalBonusTasks >=
+      0.2;
+
+    let successStatus;
+    if (isSuccessful) {
+      successStatus = "Yes";
+    } else if (!canStillSucceedRegularTasks || !canStillSucceedBonusTasks) {
+      successStatus = "Cannot succeed";
+    } else {
+      successStatus = "in progress";
+    }
+
+    return {
+      ...patient,
+      completedTasks,
+      completedTasksPercent,
+      completedRegularTasksPercent,
+      completedBonusTasksPercent,
+      successStatus,
+      openTasksCount,
+    };
+  });
+      const columns = [
+        {
+          field: "fullName",
+          headerName: "Name",
+          flex: 1,
+          cellClassName: "name-column--cell",
+        },
+        {
+          field: "id",
+          headerName: "ID",
+          flex: 0.5,
+          headerAlign: "center",
+          align: "center",
+        },
+        {
+          field: "completedRegularTasksPercent",
+          headerName: "Completed Regular Tasks %",
+          type: "number",
+          headerAlign: "center",
+          align: "center",
+          valueFormatter: (params) => `${Math.round(params.value)}%`,
+        },
+        {
+          field: "completedBonusTasksPercent",
+          headerName: "Completed Bonus Tasks %",
+          type: "number",
+          headerAlign: "center",
+          align: "center",
+          valueFormatter: (params) => `${Math.round(params.value)}%`,
+        },
+    
+        {
+          field: "completedTasksPercent",
+          headerName: "Completed Tasks %",
+          type: "number",
+          headerAlign: "center",
+          align: "center",
+          valueFormatter: (params) => `${Math.round(params.value)}%`,
+        },
+    
+        {
+          field: "openTasksCount",
+          headerName: "Open Tasks",
+          type: "number",
+          headerAlign: "center",
+          align: "center",
+        },
+    
+        {
+          field: "stage",
+          headerName: "Stage",
+          type: "number",
+          headerAlign: "center",
+          align: "center",
+        },
+    
+        {
+          field: "completedTasks",
+          headerName: "Total Tokens",
+          type: "number",
+          headerAlign: "center",
+          align: "center",
+        },
+    
+        {
+          field: "successStatus",
+          headerName: "Success Status",
+          headerAlign: "center",
+          align: "center",
+        },
+      ];
       
       
   
@@ -114,6 +269,45 @@ const PatientInfo = ()=> {
   </Grid>
 </Grid>
   
+<Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${colors.grey[100]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          rows={patientsWithCompletedTasksPercent}
+          key={(row) => row.id}
+          columns={columns}
+          components={{ Toolbar: GridToolbar }}
+        />
+      </Box>
     
   </Box>
 
